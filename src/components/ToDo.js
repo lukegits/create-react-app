b61c2eda7f522143c387aa3685fee7bfc85adf3f
Lukegits <luket.chavez@gmail.com>
2018-03-10 08:48:27 -0700

    checkpoint work todoevents

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,7 @@
  render() {
     return (
       <li>
-         <input type="checkbox" checked={ this.props.isCompleted } />
+          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
        </li>
     );
